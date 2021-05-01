e97c86ce439d6f3b4f52bd20dfa3941e2d3ec391
ca45382 <ca45382@gmail.com>
2021-05-01 20:27:52 +0900

    タイムライン変換器のファイル作成

---
+++ Script/TimeLineConversion.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Discord;
+
+namespace PriconneBotConsoleApp.Script
+{
+    class TimeLineConversion
+    {
+        public TimeLineConversion(IMessage message, int timeSec)
+        {
+
+            if (message == null)
+            {
+                return;
+            }
+
+            var messageData = loadTimeLineMessage(message);
+
+        }
+
+        private string loadTimeLineMessage(IMessage message)
+        {
+
+
+            return null;
+        }
+
+        private string ConversionMessage(string messageData, int timeData)
+        {
+            var messageContent =
+                messageData.Split(new string[] { "\n" },StringSplitOptions.None);
+
+            return null;
+        }
+    }
+}
