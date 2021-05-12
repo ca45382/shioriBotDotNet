a65fefa3740d36dee4d73f526a8973ead957fe4c
ca45382 <ca45382@gmail.com>
2021-05-12 22:02:45 +0900

    ディレクトリが存在しているか判別してなかったら作

--- Script/BotInitialize.cs
+++ Script/BotInitialize.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace PriconneBotConsoleApp.Script
 {
     class BotInitialize
     {
+        private readonly string DataFolderPath = Path.Combine("data");
+        private readonly string CacheFolderPath = Path.Combine("cache");
+        public BotInitialize()
+        {
+            if ( !Directory.Exists(DataFolderPath) )
+            {
+                Directory.CreateDirectory(DataFolderPath);
+            }
+
+            if (!Directory.Exists(CacheFolderPath))
+            {
+                Directory.CreateDirectory(CacheFolderPath);
+            }
+        }
+
 
     }
 }
