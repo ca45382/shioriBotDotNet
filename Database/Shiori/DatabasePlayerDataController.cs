65ef9bbe899cfac3a8cbde93056024751d842309
ca45382 <ca45382@gmail.com>
2021-06-06 00:59:53 +0900

    改良

--- Database/Shiori/DatabasePlayerDataController.cs
+++ Database/Shiori/DatabasePlayerDataController.cs
@@ -36,10 +36,14 @@ namespace PriconneBotConsoleApp.Database
             foreach (PlayerData playerData in playersData)
             {
                 var clanID = databaseConnector.ClanData
-                    .Include(d => d.ServerData)
-                    .Where(d => d.ServerID == playerData.ClanData.ServerID && d.ClanRoleID == playerData.ClanData.ClanRoleID)
-                    .Select(d => d.ClanID)
-                    .FirstOrDefault();
+                    .FirstOrDefault(d => d.ServerID == playerData.ClanData.ServerID && d.ClanRoleID == playerData.ClanData.ClanRoleID)
+                    ?.ClanID ?? 0;
+
+                if (clanID == 0)
+                {
+                    transaction.Rollback();
+                    return;
+                }
 
                 var newPlayerData = new PlayerData
                 {
