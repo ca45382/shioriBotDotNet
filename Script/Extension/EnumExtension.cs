0619c056c48dcd24176fe7d588b87154b57eff10
ca45382 <ca45382@gmail.com>
2021-06-06 22:50:35 +0900

    EnumのExtensionを追加

---
+++ Script/Extension/EnumExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+//using PriconneBotConsoleApp.Enum;
+
+namespace PriconneBotConsoleApp.Script
+{
+    public static class EnumExtension
+    {
+        public static string GetDescription(this System.Enum type)
+        {
+            var descriptionAttribute = type.GetType()
+               .GetField(type.ToString()).GetCustomAttribute<DescriptionAttribute>(false);
+
+            return descriptionAttribute?.Description;
+        }
+    }
+}
