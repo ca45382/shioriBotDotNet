cb333e704876bfe1b41ea3788a29d23a22b4ad8b
ca45382 <ca45382@gmail.com>
2021-06-19 01:08:41 +0900

    ClanDataExtensionの削除に伴う変更

--- DataModel/Shiori/ClanData.cs
+++ DataModel/Shiori/ClanData.cs
@@ -53,6 +53,99 @@ namespace PriconneBotConsoleApp.DataModel
         public List<MessageData> MessageData { get; set; }
         public List<RoleData> roleData { get; set; }
         public List<PlayerData> PlayerData { get; set; }
+
+        public int GetBossLap(int bossNumber)
+        {
+            return bossNumber switch
+            {
+                1 => Boss1Lap,
+                2 => Boss2Lap,
+                3 => Boss3Lap,
+                4 => Boss4Lap,
+                5 => Boss5Lap,
+                _ => 0,
+            };
+        }
+
+        public void SetBossLap(int bossNum, int bossLap)
+        {
+            switch (bossNum)
+            {
+                case 1:
+                    Boss1Lap = (ushort)bossLap;
+                    return;
+                case 2:
+                    Boss2Lap = (ushort)bossLap;
+                    return;
+                case 3:
+                    Boss3Lap = (ushort)bossLap;
+                    return;
+                case 4:
+                    Boss4Lap = (ushort)bossLap;
+                    return;
+                case 5:
+                    Boss5Lap = (ushort)bossLap;
+                    return;
+                default:
+                    return;
+            }
+        }
+
+        /// <summary>
+        /// 5つのボスデータから今のボスに変換。来月削除。
+        /// </summary>
+        /// <param name="clanData"></param>
+        /// <returns></returns>
+        [Obsolete]
+        public byte GetNowBoss()
+        {
+            if (Boss1Lap == Boss2Lap
+                && Boss2Lap == Boss3Lap
+                && Boss3Lap == Boss4Lap
+                && Boss4Lap == Boss5Lap)
+            {
+                return 1;
+            }
+            if (Boss1Lap - 1 == Boss2Lap)
+            {
+                return 2;
+            }
+            else if (Boss2Lap - 1 == Boss3Lap)
+            {
+                return 3;
+            }
+            else if (Boss3Lap - 1 == Boss4Lap)
+            {
+                return 4;
+            }
+            else if (Boss4Lap - 1 == Boss5Lap)
+            {
+                return 5;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// 5つのボスデータから今のLapに変換。来月削除。
+        /// </summary>
+        /// <param name="clanData"></param>
+        /// <returns></returns>
+        [Obsolete]
+        public ushort GetNowLap()
+        {
+            if (Boss1Lap == Boss2Lap
+                && Boss2Lap == Boss3Lap
+                && Boss3Lap == Boss4Lap
+                && Boss4Lap == Boss5Lap)
+            {
+                return Boss1Lap;
+            }
+            else
+            {
+                return Boss5Lap;
+            }
+        }
     }
 
 }
