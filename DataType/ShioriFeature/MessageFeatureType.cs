8b80db0158c6071466a583b709e3df6f7ea41311
ca45382 <ca45382@gmail.com>
2021-06-19 23:31:30 +0900

    不要なDescriptionを削除

--- DataType/ShioriFeature/MessageFeatureType.cs
+++ DataType/ShioriFeature/MessageFeatureType.cs
@@ -5,49 +5,24 @@ namespace PriconneBotConsoleApp.DataType
     //メッセージ 2xxx
     public enum MessageFeatureType
     {
-        [Description("")]
         Unknown = 0,
 
-        [Description("DeclareID")]
         DeclareID = 2001,
-
-        [Description("ReserveResultID")]
         ReserveResultID = 2002,
-
-        [Description("ProgressID")]
         ProgressID = 2003,
 
-
         //凸宣言チャンネルメッセージ 210x
-        [Description("DeclareBoss1ID")]
         DeclareBoss1ID = 2101,
-
-        [Description("DeclareBoss2ID")]
         DeclareBoss2ID = 2102,
-
-        [Description("DeclareBoss3ID")]
         DeclareBoss3ID = 2103,
-
-        [Description("DeclareBoss4ID")]
         DeclareBoss4ID = 2104,
-
-        [Description("DeclareBoss5ID")]
         DeclareBoss5ID = 2105,
 
         //進行チャンネルメッセージ 211x
-        [Description("ProgressBoss1ID")]
         ProgressBoss1ID = 2111,
-
-        [Description("ProgressBoss1ID")]
         ProgressBoss2ID = 2112,
-
-        [Description("ProgressBoss1ID")]
         ProgressBoss3ID = 2113,
-
-        [Description("ProgressBoss1ID")]
         ProgressBoss4ID = 2114,
-
-        [Description("ProgressBoss1ID")]
         ProgressBoss5ID = 2115,
     }
 }
--- DataType/ShioriFeature/RoleFeatureType.cs
+++ DataType/ShioriFeature/RoleFeatureType.cs
@@ -5,25 +5,14 @@ namespace PriconneBotConsoleApp.DataType
     //ロール系 3xxx
     public enum RoleFeatureType
     {
-        [Description("")]
         Unknown = 0,
 
-        [Description("TaskKillRoleID")]
         TaskKillRoleID = 3001,
 
-        [Description("Boss1RoleID")]
         Boss1RoleID = 3101,
-
-        [Description("Boss1RoleID")]
         Boss2RoleID = 3102,
-
-        [Description("Boss1RoleID")]
         Boss3RoleID = 3103,
-
-        [Description("Boss1RoleID")]
         Boss4RoleID = 3104,
-
-        [Description("Boss1RoleID")]
         Boss5RoleID = 3105,
     }
 }
