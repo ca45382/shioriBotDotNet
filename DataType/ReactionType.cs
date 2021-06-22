7d7f307b414d420af4b565531ed041366ec7c67e
ca45382 <ca45382@gmail.com>
2021-06-22 22:40:24 +0900

    リアクションタイプの追加

--- DataType/ReactionType.cs
+++ DataType/ReactionType.cs
@@ -5,6 +5,24 @@ namespace PriconneBotConsoleApp.DataType
     public enum ReactionType
     {
         [Description("👌")]
-        Success = 1,
+        Success = 101,
+
+        [Description("❌")]
+        Failure = 102,
+
+        [Description("🔄")]
+        Reload = 103,
+
+        [Description("⚔️")]
+        StartBattle = 1001,
+
+        [Description("✅")]
+        FinishBattle = 1002,
+
+        [Description("🏁")]
+        SubdueBoss = 1003,
+
+        [Description("❌")]
+        CancelBattle = 1004,
     }
 }
