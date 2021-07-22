cc56e6c6edc28af244a0a60a4785d265aa31df78
ca45382 <ca45382@gmail.com>
2021-07-23 01:31:33 +0900

    ZenToHanを改善

--- Extension/StringExtension.cs
+++ Extension/StringExtension.cs
@@ -7,8 +7,9 @@ namespace PriconneBotConsoleApp.Extension
         public static string ZenToHan(this string textData)
         {
             var convertText = textData;
-            convertText = Regex.Replace(convertText, "　", p => ((char)(p.Value[0] - '　' + ' ')).ToString());
-            convertText = Regex.Replace(convertText, "＠", p => ((char)(p.Value[0] - '＠' + '@')).ToString());
+            convertText = convertText.Replace('　', ' ');
+            convertText = convertText.Replace('＠', '@');
+            convertText = convertText.Replace('！', '!');
             convertText = Regex.Replace(convertText, "[０-９]", p => ((char)(p.Value[0] - '０' + '0')).ToString());
             convertText = Regex.Replace(convertText, "[ａ-ｚ]", p => ((char)(p.Value[0] - 'ａ' + 'a')).ToString());
             convertText = Regex.Replace(convertText, "[Ａ-Ｚ]", p => ((char)(p.Value[0] - 'Ａ' + 'A')).ToString());
