bfa120a54e9bd3d13b0bc7a6e6db0e02484b17a3
ca45382 <ca45382@gmail.com>
2021-07-25 20:58:05 +0900

    新規BotConfigDataのクラスを作成

---
+++ DataModel/BotConfigData.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PriconneBotConsoleApp.DataModel
+{
+    public class BotConfigData
+    {
+        [JsonPropertyName("discord")]
+        public DiscordConfig DiscordConfig { get; set; }
+
+        [JsonPropertyName("database")]
+        public DatabaseConfig DatabaseConfig { get; set; }
+    }
+
+    public class DiscordConfig
+    {
+        [JsonPropertyName("token")]
+        public string Token { get; set; }
+    }
+
+    public class DatabaseConfig
+    {
+        [JsonPropertyName("host")]
+        public string Host { get; set; }
+
+        [JsonPropertyName("port")]
+        public int Port { get; set; }
+
+        [JsonPropertyName("user")]
+        public string User { get; set; }
+
+        [JsonPropertyName("password")]
+        public string Password { get; set; }
+
+        [JsonPropertyName("database")]
+        public string Database { get; set; }
+
+        [JsonPropertyName("sslmode")]
+        public string SSLMode { get; set; }
+    }
+}
