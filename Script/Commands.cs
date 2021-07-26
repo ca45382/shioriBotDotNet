3d8a1fb22215fafe1a38b516f8b90e12407f7cd8
ca45382 <ca45382@gmail.com>
2021-07-27 03:18:43 +0900

    コマンド処理部分の作成

---
+++ Script/Commands.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using PriconneBotConsoleApp.Attribute;
+using PriconneBotConsoleApp.DataModel;
+using PriconneBotConsoleApp.DataType;
+using PriconneBotConsoleApp.Extension;
+
+namespace PriconneBotConsoleApp.Script
+{
+    public static class Commands
+    {
+        [Command(
+            channelFeatureType: new[] { ChannelFeatureType.CarryOverID }
+        )]
+        public static async Task BattleCarryOver(CommandEventArgs commandEventArgs)
+            => await new BattleCarryOver(commandEventArgs.ClanData, commandEventArgs.SocketUserMessage).RunByMessage();
+
+        [Command(
+            channelFeatureType: new[]{ 
+                ChannelFeatureType.ProgressBoss1ID,
+                ChannelFeatureType.ProgressBoss2ID,
+                ChannelFeatureType.ProgressBoss3ID,
+                ChannelFeatureType.ProgressBoss4ID,
+                ChannelFeatureType.ProgressBoss5ID,
+            }
+        )]
+        public static async Task Progress(CommandEventArgs commandEventArgs)
+            => await new BattleProgress(commandEventArgs.ClanData, commandEventArgs.SocketUserMessage, (byte)commandEventArgs.ChannelFeatureType.GetBossNumberType())
+                .RunByMessage();
+    }
+}
