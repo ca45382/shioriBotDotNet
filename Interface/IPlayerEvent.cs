d8e1ec41b1cceaf679c3f8e057c68b4ef951a26e
ca45382 <ca45382@gmail.com>
2021-08-09 21:47:58 +0900

    ボタンの汎用解釈とインタフェースを作成

---
+++ Interface/IPlayerEvent.cs
@@ -0,0 +1,16 @@
+﻿using Discord.WebSocket;
+using PriconneBotConsoleApp.DataModel;
+using PriconneBotConsoleApp.DataType;
+
+namespace PriconneBotConsoleApp.Interface
+{
+    public interface IPlayerEvent
+    {
+        public SocketGuildUser User { get; }
+        public SocketTextChannel Channel { get; }
+        public SocketRole Role { get; }
+        public ClanData ClanData { get; }
+        public PlayerData PlayerData { get; }
+        public ChannelFeatureType ChannelFeatureType { get; }
+    }
+}
---
+++ Model/ButtonEvent.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Discord.WebSocket;
+using PriconneBotConsoleApp.Database;
+using PriconneBotConsoleApp.DataType;
+using PriconneBotConsoleApp.Interface;
+
+namespace PriconneBotConsoleApp.DataModel
+{
+    public class ButtonEvent : IPlayerEvent
+    {
+        public ButtonEvent(SocketInteraction socketInteraction)
+        {
+            SocketMessageComponent = (SocketMessageComponent)socketInteraction;
+            User = socketInteraction.User as SocketGuildUser;
+            Channel = socketInteraction.Channel as SocketTextChannel;
+            PlayerData = DatabasePlayerDataController.LoadPlayerData(Channel.Guild.Id, User.Id);
+            Role = Channel.Guild.GetRole(PlayerData?.ClanData.ClanRoleID ?? 0);
+
+            if (Role == null)
+            {
+                return;
+            }
+
+            ClanData = DatabaseClanDataController.LoadClanData(Role);
+            ChannelFeatureType = (ChannelFeatureType?)ClanData.ChannelData.FirstOrDefault(x => x.ChannelID == Channel.Id)?.FeatureID 
+                ?? ChannelFeatureType.All;
+
+        }
+
+        public SocketMessageComponent SocketMessageComponent { get; }
+        public ButtonType ButtonType { get; }
+        public SocketGuildUser User { get; }
+        public SocketTextChannel Channel { get; }
+        public SocketRole Role { get; }
+        public ClanData ClanData { get; }
+        public PlayerData PlayerData { get; }
+        public ChannelFeatureType ChannelFeatureType { get; }
+    }
+}
