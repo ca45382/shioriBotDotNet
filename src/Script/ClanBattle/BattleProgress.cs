69302ac8e899753b7053835ed2a5f6ebcb657368
ca45382 <ca45382@gmail.com>
2021-09-14 21:40:32 +0900

    冗長な部分を修正

--- src/Script/ClanBattle/BattleProgress.cs
+++ src/Script/ClanBattle/BattleProgress.cs
@@ -529,7 +529,7 @@ namespace ShioriBot.Net.Script
                 BattleLap = (ushort)m_CommandEventArgs.ClanData.GetBossLap(m_BossNumberType),
                 BossNumber = (byte)m_BossNumberType,
                 PlayerID = progressData.PlayerID,
-                RemainTime = (byte)(progressData.RemainTime),
+                RemainTime = progressData.RemainTime,
             };
 
             DatabaseCarryOverController.CreateCarryOverData(carryOverData);
