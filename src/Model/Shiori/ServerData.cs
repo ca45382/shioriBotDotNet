6617db2b77aa1b89ae8d59de7fe8e3d009cd57cb
ca45382 <ca45382@gmail.com>
2023-06-23 00:27:56 +0900

    サーバーデータにカラムを追加

--- src/Model/Shiori/ServerData.cs
+++ src/Model/Shiori/ServerData.cs
@@ -1,14 +1,24 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.CompilerServices;
+using Microsoft.EntityFrameworkCore;
 
 namespace ShioriBot.Model
 {
     [Table("server_data")]
     public class ServerData
     {
-        [Column("server_id", TypeName = "BIGINT UNSIGNED"), Key]
+        [Column("server_id", TypeName = "BIGINT UNSIGNED AUTO_INCREMENT"), Key, Required]
         public ulong ServerID { get; set; }
+
+        /// <summary>
+        /// DiscordのサーバーID
+        /// </summary>
+        [Column("discord_guild_id", TypeName = "BIGINT UNSIGNED"), Required]
+        public ulong DiscordGuildID { get; set; }
 
         [Column("server_name", TypeName = "VARCHAR(100)")]
         public string ServerName { get; set; }
@@ -28,6 +38,20 @@ namespace ShioriBot.Model
         [Column("system_ch_id", TypeName = "BIGINT UNSIGNED")]
         public ulong SystemCommandChannelID { get; set; }
 
+        [Column("create_date_time")]
+        [Timestamp]
+        public DateTime CreateDateTime { get; set; }
+
+        [Column("delete_flag", TypeName = "TINYINT UNSIGNED"), DefaultValue(false)]
+        public bool DeleteFlag { get; set; }
+
         public List<ClanData> ClanData { get; set; }
+
+        public static void OnModelCreate(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ServerData>()
+                .HasIndex(m => m.DiscordGuildID)
+                .IsUnique();
+        }
     }
 }
